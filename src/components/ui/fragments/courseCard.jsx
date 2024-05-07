ec576f11d7d3899155716c9219125a0998352a7a
takachan0012 <jj4068851@gmail.com>
2024-05-07 08:21:06 +0700

    feat: add react-lazy-load-image-component

--- src/components/ui/fragments/courseCard.jsx
+++ src/components/ui/fragments/courseCard.jsx
@@ -10,13 +10,18 @@ import avatar1 from "@assets/avatar/image-1.png";
 import avatar2 from "@assets/avatar/image-2.png";
 import avatar3 from "@assets/avatar/image-3.png";
 import avatar4 from "@assets/avatar/image-4.png";
-
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const CourseCard = forwardRef(({ className, ...props }, ref) => (
     <Card
         className={cn("w-[405px] bg-transparent h-[457px] rounded-[4px] border-t-2 border-r-4 border-b-4 border-l-2 border-[#ffffff08] p-0", className)}>
         <CardHeader className="bg-[#ffffff4d] rounded-[4px] p-0 h-[233px] relative overflow-hidden">
-            <img src={props.image} alt={`icon-${props.title}`} className="h-[450px] w-[430px] absolute top-0 left-0 hover:scale-150 transition-all delay-100" />
+            <LazyLoadImage
+                src={props.image}
+                alt={`icon-${props.title}`}
+                className="h-[450px] w-[430px] absolute top-0 left-0"
+                effect="blur" />
         </CardHeader>
         <CardContent className="w-[405px] h-[224px] p-[24px]">
             <section id="total-member" className="w-[357px] h-[42px] flex justify-between items-center">
