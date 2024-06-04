785c22aa54fe0e744d8effd3156468fcabf9e718
takachan0012 <jj4068851@gmail.com>
2024-06-04 16:46:14 +0700

    feat: add text-shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,11 +27,44 @@ module.exports = {
           to: { height: "0" },
         },
       },
+      textShadow: {
+        'sm': '2px 1px black,-1px 0px black',
+        'md': '0 4px 6px rgba(0, 0, 0, 0.1)',
+        'lg': '0 10px 15px rgba(0, 0, 0, 0.15)',
+        'xl': '0 20px 25px rgba(0, 0, 0, 0.2)',
+        '2xl': '0 25px 50px rgba(0, 0, 0, 0.25)',
+        'none': 'none',
+      },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"),
+  function ({ addUtilities }) {
+    const newUtilities = {
+      '.text-shadow-sm': {
+        textShadow: '2px 1px black,-1px 0px black',
+      },
+      '.text-shadow-md': {
+        textShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+      },
+      '.text-shadow-lg': {
+        textShadow: '0 10px 15px rgba(0, 0, 0, 0.15)',
+      },
+      '.text-shadow-xl': {
+        textShadow: '0 20px 25px rgba(0, 0, 0, 0.2)',
+      },
+      '.text-shadow-2xl': {
+        textShadow: '0 25px 50px rgba(0, 0, 0, 0.25)',
+      },
+      '.text-shadow-none': {
+        textShadow: 'none',
+      },
+    }
+
+    addUtilities(newUtilities, ['responsive', 'hover'])
+  }
+  ],
 }
