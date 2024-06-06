3655ef6f505bc1340d5f592f57e3a1e7d353885e
takachan0012 <jj4068851@gmail.com>
2024-06-06 20:38:57 +0700

    feat: add state isOpen to trigger bg-color accrodion root

--- src/components/ui/fragments/faq.jsx
+++ src/components/ui/fragments/faq.jsx
@@ -1,23 +1,36 @@
-import React, { forwardRef } from "react";
+import React, { useState } from "react";
 import {
     Accordion,
     AccordionItem,
     AccordionTrigger,
     AccordionContent,
 } from "@/components/ui/accordion"
+import { cn } from "@/lib/utils";
 
-const Faq = forwardRef(({ className, ...props }, ref) => (
-    <Accordion type="single" collapsible className="w-full border-t-2 border-r-4 border-b-4 border-l-2 border-[#101010]">
-        <AccordionItem value={props.value}>
-            <AccordionTrigger>What is the purpose of this website?</AccordionTrigger>
-            <AccordionContent>
-                <p>
-                    This website is designed to provide information about the different types of
-                </p>
-            </AccordionContent>
-        </AccordionItem>
-    </Accordion>
-))
+const Faq = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <Accordion type="single"
+            collapsible
+            className={cn("border-t-2 border-r-4 border-b-4 border-l-2 border-[#101010] p-6 w-[624px] h-fit", isOpen ? "bg-[#FFB81E]" : "")}
+        >
+            <AccordionItem
+                value={props.value}
+                className="border-b-0 data-[state=open]:bg-[#FFB81E]"
+            >
+                <AccordionTrigger
+                    className="font-bold text-[20px] leading-7"
+                    onClick={() => setIsOpen(!isOpen)}
+                >What is the purpose of this website?</AccordionTrigger>
+                <AccordionContent className="border-b-0 border-t-2 p-6">
+                    <p>
+                        This website is designed to provide information about the different types of
+                    </p>
+                </AccordionContent>
+            </AccordionItem>
+        </Accordion >
+    )
+}
 
 Faq.displayName = "Faq"
 
