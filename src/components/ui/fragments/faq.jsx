e21711673a9d49a14701286b838aa39978f693b3
takachan0012 <jj4068851@gmail.com>
2024-06-07 14:32:56 +0700

    feat: add button plus minus

--- src/components/ui/fragments/faq.jsx
+++ src/components/ui/fragments/faq.jsx
@@ -7,6 +7,7 @@ import {
 } from "@/components/ui/accordion"
 import { cn } from "@/lib/utils";
 
+
 const Faq = (props) => {
     const [isOpen, setIsOpen] = useState(false);
     return (
@@ -21,7 +22,18 @@ const Faq = (props) => {
                 <AccordionTrigger
                     className="font-bold text-[20px] leading-7"
                     onClick={() => setIsOpen(!isOpen)}
-                >{props.title}</AccordionTrigger>
+                >
+                    {props.title}
+                    <div className={cn("h-[36px] w-[36px] border-t border-r-4 border-b-4 border-l border-[#101010] flex items-center justify-center rounded-[4px]", isOpen ? "bg-white" : "bg-[#FFB81E]")}>
+                        {isOpen ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14" />
+                        </svg> : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                        </svg>
+                        }
+                    </div>
+
+                </AccordionTrigger>
                 <AccordionContent className="border-b-0 border-t  pt-6 border-[#101010]">
                     <p className="font-bold leading-[25px]">
                         {props.content}
