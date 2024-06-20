1726b5d5d15a5c68adc1d77a7612c84be2443750
takachan0012 <jj4068851@gmail.com>
2024-06-20 20:30:57 +0700

    feat: overflow-x-hidden

--- src/components/sections/root.jsx
+++ src/components/sections/root.jsx
@@ -1,7 +1,7 @@
 import { forwardRef } from "react";
 import { cn } from "@/lib/utils";
 const Root = forwardRef(({ className, ...props }, ref) => (
-    <div className={cn("lg:h-screen", className)} ref={ref} {...props} />
+    <div className={cn("w-screen overflow-x-hidden lg:h-screen", className)} ref={ref} {...props} />
 ));
 
 Root.displayName = "Root";
